e0660317ec9f81a915b7c4e6cc06c45d1ebf0d07
2022057 <2022057@student.cct.ie>
2023-05-15 14:53:11 +0100

    adding service

--- contactsApp/src/app/home/home.page.ts
+++ contactsApp/src/app/home/home.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { StorageService } from './services/storage.services';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  person = {name:'', phone:''};
+  name!: string;
+  storageName!: string;
+  phone!: string;
 
-  constructor() {}
 
-}
+  constructor(
+    private storage: StorageService
+  ) {}
+
+  setStorage() {
+    this.storage.setString('name',this.name);
+    this.storage.setObject('person', {
+      name: this.name,
+      phone: this.phone
+    });
+  }
+
+  getStorage() {
+   
+    this.storage.getString('name').then((data:any)=>{
+      if (data.value){
+        this.storageName = data.value;
+      }
+    });
+
+    this.storage.getObject('person').then((data: any)=>{
+      this.person = data;
+    });
+        
+  }
+
+    clearStorage() {
+      this.storage.clear();
+    } 
+  }

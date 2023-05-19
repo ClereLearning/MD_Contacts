26c3ee257d9f179a73c0aed38d6b4731f64916db
2022057 <2022057@student.cct.ie>
2023-05-19 19:38:32 +0100

    reorganizing names, structure, layout , comments

--- contactsApp/src/app/home/services/studentstorage.services.ts
+++ contactsApp/src/app/home/services/studentstorage.services.ts
@@ -5,34 +5,34 @@ import { Storage } from "@capacitor/storage";
     providedIn: 'root'
 })
 
-export class StorageService {
+export class StudentStorage {
     constructor(){}
 
-    async setString(key:string, value:string)
-    {
-        await Storage.set({key,value});
-    }
-
-    async getString(key:string): Promise<{value: any}>{
-        return (await Storage.get({key}));
-    }
-
+    // to save any object in this case student
     async setObject(key:string, value:any){               
         await Storage.set({key, value: JSON.stringify(value)});      
     }
 
+    // to get any object in this case student
     async getObject(key: string): Promise<{value: any}> {
         const ret = await Storage.get({key});
         
         return ret.value? JSON.parse(ret.value):null;
     }
 
-    async removeItem(key: string)
+    // to save the dictionary with the key name 
+    async setString(key:string, value:string)
     {
-        await Storage.remove({key});
+        await Storage.set({key,value});
     }
 
-    async clear()
+    // to get the saved information from storage like a dictionary 
+    async getString(key:string): Promise<{value: any}>{
+        return (await Storage.get({key}));
+    }
+
+    // to clear all information in the storage
+    async clearStorage()
     {
         await Storage.clear();
     }
